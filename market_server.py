69642f88f6ebb4de6531c806dd258373ca7b1da1
ferreret <ferreret@gmail.com>
2025-10-21 11:26:19 +0200

    Versión del curso

---
+++ market_server.py
@@ -0,0 +1,19 @@
+from market import get_share_price
+
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("market_server")
+
+
+@mcp.tool()
+async def lookup_share_price(symbol: str) -> float:
+    """Esta herramienta proporciona el precio actual del símbolo de acción dado.
+
+    Argumentos:
+        symbol: el símbolo de la acción
+    """
+    return get_share_price(symbol)
+
+
+if __name__ == "__main__":
+    mcp.run(transport="stdio")
